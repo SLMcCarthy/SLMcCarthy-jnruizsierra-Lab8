d29df8ee10c2636246c8a768b53bb1da7eb24502
slmccarthy <slmccarthy@loyola.edu>
2020-10-27 12:51:32 -0400

    Created files

---
+++ Number.java
@@ -0,0 +1,11 @@
+/** This is our code, it stores a node as  a number
+ * C312	- Lab 8
+ * @authors Shane McCarthy, Jomaly Ruiz Sierra, Christina Lutton
+ */
+
+public class Number
+{
+    int num;
+    Number left, right;
+
+}
